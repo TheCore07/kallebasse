76189a3dbb20906151f0b1158ad08b54959a135c
TheCoreß7 <fabianaltaner@gmail.com>
2025-11-24 21:35:01 +0100

    fixed building bugs

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -45,7 +45,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     };
 
     useEffect(() => {
-        setupAxiosInterceptors(logout); // nur einmal
+        setupAxiosInterceptors(); // nur einmal
         refreshUser();
     }, []);
 
